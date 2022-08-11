24a25bf2dee8055db939735dcbfb50a3f7f2e60c
DmitroYakimov <56223340+DmitroYakimov@users.noreply.github.com>
2022-08-11 17:32:18 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+import { FooterContainer } from './containers/footer'
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <Navbar />
+            <Hero />
+            <FooterContainer />
+        </>
+    );
+}
+
+export default App;
